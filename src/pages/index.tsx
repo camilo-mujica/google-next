09057eeb2017f8bc6aaaa97d798b9d843ffd8a0a
camilo <camilo_mujica@hotmail.com>
2023-05-31 20:29:58 -0400

    added eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 export default function HomePage() {
-    return (
-        <>
-            <div>
-                Home
-                <input type="text" />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        Home
+        <input type="text" />
+      </div>
+    </>
+  )
 }
--- src/pages/resultados.tsx
+++ src/pages/resultados.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const ResultsPage = () => {
-  return (
-    <div>ResultsPage</div>
-  )
+  return <div>ResultsPage</div>
 }
 
 export default ResultsPage
