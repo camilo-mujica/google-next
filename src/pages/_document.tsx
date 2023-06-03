26477380e75d049b1c5b79bc075c5cb22191039b
camilo <camilo_mujica@hotmail.com>
2023-06-03 09:39:41 -0400

    updated pagination logo font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,7 @@ class MyDocument extends Document {
                         crossOrigin=""
                     />
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@300;400;500;600;700;800&family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
                         rel="stylesheet"
                     />
                 </Head>
